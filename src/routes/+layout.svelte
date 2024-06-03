0a368cc17deea5efbb0d7dd4198d70baffcf43f5
gsovereignty <gsovereignty@proton.me>
2024-06-03 18:17:16 +0800

    problem: no starting point

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+<ModeWatcher />
+<slot></slot>
+
+<style></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Sidebar from "../layouts/Sidebar.svelte";
+
+</script>
+<Sidebar />
