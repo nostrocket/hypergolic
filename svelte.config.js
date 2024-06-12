577e48af48022a63f54d7ae400d57919977ffaa9
gsovereignty <gsovereignty@proton.me>
2024-06-12 15:53:49 +0800

    problem: can't check dev mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
