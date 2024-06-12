f25a51b509f2bfbc2b3f7fee6dc15d355617d950
gsovereignty <gsovereignty@proton.me>
2024-06-12 15:38:10 +0800

    problem: can't build for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,6 +14,12 @@ const config = {
 		adapter: adapter({
 			fallback: "404.html",
 		  }),
+		  prerender: {
+			crawl: true,
+		  },
+		  paths: {
+			base: "",
+		  },
 		alias: {
 			"@/*": "./src/lib/*",
 			"@components/*": "./src/lib/components/*",
