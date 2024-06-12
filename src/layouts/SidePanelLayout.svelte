81678267aaf67f894409c5a215bafdfcecf8cc69
gsovereignty <gsovereignty@proton.me>
2024-06-12 17:34:37 +0800

    problem: people expect too much

--- src/layouts/SidePanelLayout.svelte
+++ src/layouts/SidePanelLayout.svelte
@@ -67,6 +67,12 @@
             </div>
           </Sheet.Content>
         </Sheet.Root>
+
+          <div class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+            <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">Alert</span>
+            <span class="font-semibold mr-2 text-left flex-auto">Nostrocket is so not ready yet, this is just a preview</span>
+          </div>
+
         <div class="w-full flex-1">
           <form>
             <div class="relative">
