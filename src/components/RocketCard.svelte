c9ea5b28c911258662788ebc21a6f3909ef707c1
gsovereignty <gsovereignty@proton.me>
2024-07-03 17:09:16 +0800

    problem: no starting point for building rocket dashboard

--- src/components/RocketCard.svelte
+++ src/components/RocketCard.svelte
@@ -4,6 +4,7 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import type { NDKEvent } from '@nostr-dev-kit/ndk';
+	import { ChevronRight } from 'lucide-svelte';
 
 	export let rocketEvent: NDKEvent;
     //$page.url.searchParams.get("tab")
@@ -30,6 +31,6 @@
 	<Card.Content></Card.Content>
 	<Card.Footer class="flex justify-between">
         <Button on:click={()=>{console.log(rocketEvent.rawEvent())}} variant="outline">Print to Console</Button>
-		<Button on:click={()=>{goto(`${base}/rockets/${getRocketURL(rocketEvent)}`)}}>View Full Rocket</Button>
+		<Button on:click={()=>{goto(`${base}/rockets/${getRocketURL(rocketEvent)}`)}}>View Full Rocket<ChevronRight class="h-4 w-4" /></Button>
 	</Card.Footer>
 </Card.Root>
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
       
     import EmptyList from "../../components/EmptyList.svelte";
+	import RocketDashboard from "../../components/RocketDashboard.svelte";
+    //<EmptyList />
   </script>
-<EmptyList />
+<RocketDashboard />
