fbd4532cc161805d796933ba89cad610bcb7c9a8
gsovereignty <gsovereignty@proton.me>
2024-07-29 16:52:16 +0800

    problem: fetching event sometimes fails

--- src/components/ProductCardFromID.svelte
+++ src/components/ProductCardFromID.svelte
@@ -11,9 +11,18 @@
 		$ndk.fetchEvent(productID).then((e) => {
 			if (e) {
 				productEvent = e;
+			} else {
+				let _p = $ndk.storeSubscribe([{ids:[productID] }], { subId: productID });
+				_p.subscribe(x=>{
+					if (x[0]) {
+						productEvent = x[0]
+						_p.unsubscribe()
+					}
+				})
 			}
 		});
 	});
+
 </script>
 
 {#if productEvent}
