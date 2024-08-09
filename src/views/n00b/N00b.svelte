7fa19c01f640e4af563649ed9df831a595b989dd
Angelica Willianto <angelicawillianto@gmail.com>
2024-08-09 17:43:11 +0700

    fix: layout in main page

--- src/views/n00b/N00b.svelte
+++ src/views/n00b/N00b.svelte
@@ -21,95 +21,86 @@
 </script>
 
 <div class="">
-	<div class="flex flex-row flex-wrap gap-4">
-		<div class="flex flex-col">
-			<Card.Root class="max-w-80">
-				<Card.Header>
-					<Card.Title>Proof of Work</Card.Title>
-					<Card.Description>I have skills and I'm ready to work for sats.</Card.Description>
-				</Card.Header>
-				<img src="{base}/_work.png" />
-				<Card.Content class="grid gap-4">
-					<div>
-						<ul class="ml-6 list-disc [&>li]:mt-2">
-							<li>Solve problems for fun and profit</li>
-							<li>No meetings, no scrum, no task assignment, no schedules</li>
-							<li>Work only on what inspires you</li>
-							<li>
-								No interview process or CV's required, apply by sending a pull request to solve a
-								small problem
-							</li>
-							<li>Get paid in Sats for every pull request</li>
-						</ul>
-					</div>
-				</Card.Content>
-				<Card.Footer>
-					<Button class="w-full">Apply Now</Button>
-				</Card.Footer>
-			</Card.Root>
-		</div>
-		<div class="flex flex-col">
-			<Card.Root class="max-w-80">
-				<Card.Header>
-					<Card.Title>Proof of Sats</Card.Title>
-					<Card.Description>I have Sats, and I want a slice of a Rocket's satflow.</Card.Description
-					>
-				</Card.Header>
-				<img src="{base}/_sats.png" width="100%" />
-				<Card.Content class="grid gap-4">
-					<div>
-						<ul class="ml-6 list-disc [&>li]:mt-2">
-							<li>
-								Nostrocket is the most efficient way (ever) to invest in emerging technologies
-							</li>
-							<li>
-								Every Sat goes directly to a contributor for solving a problem in the critical path
-								towards (more) revenue
-							</li>
-							<li>
-								Revenue goes straight from the paying customer's wallet into your wallet as an
-								equity holder
-							</li>
-							<li>No KYC, ever.</li>
-						</ul>
-					</div>
-				</Card.Content>
-				<Card.Footer>
-					<Button class="w-full">Read More</Button>
-				</Card.Footer>
-			</Card.Root>
-		</div>
-		<div class="flex flex-col">
-			<Card.Root class="max-w-80">
-				<Card.Header>
-					<Card.Title>Launch a new Rocket</Card.Title>
-					<Card.Description
-						>I'm working on my own project and I want other people to join me.</Card.Description
-					>
-				</Card.Header>
-				<img src="{base}/_rocket.png" width="100%" />
-				<Card.Content class="grid gap-4">
-					<div>
-						<ul class="ml-6 list-disc [&>li]:mt-2">
-							<li>
-								Attract the people you need to make your project a success (despite your lack of
-								funding)
-							</li>
-							<li>
-								Nostrocket obsoletes the need for financial accounting, capital raising, compliance,
-								business plans, etc.
-							</li>
-							<li>
-								Nostrocket is radically meritocratic and fair to project creators, contributors, and
-								investors, for the entire life of the project
-							</li>
-						</ul>
-					</div>
-				</Card.Content>
-				<Card.Footer>
-					<CreateNewRocket />
-				</Card.Footer>
-			</Card.Root>
-		</div>
+	<div class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+		<Card.Root class="flex h-full w-full flex-col">
+			<Card.Header>
+				<Card.Title>Proof of Work</Card.Title>
+				<Card.Description>I have skills and I'm ready to work for sats.</Card.Description>
+			</Card.Header>
+			<img src="{base}/_work.png" alt="Proof of Work" />
+			<Card.Content class="grid gap-4 !pt-2">
+				<div>
+					<ul class="ml-6 list-disc [&>li]:mt-2">
+						<li>Solve problems for fun and profit</li>
+						<li>No meetings, no scrum, no task assignment, no schedules</li>
+						<li>Work only on what inspires you</li>
+						<li>
+							No interview process or CV's required, apply by sending a pull request to solve a
+							small problem
+						</li>
+						<li>Get paid in Sats for every pull request</li>
+					</ul>
+				</div>
+			</Card.Content>
+			<Card.Footer class="mt-auto">
+				<Button class="w-full">Apply Now</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root class="flex h-full w-full flex-col">
+			<Card.Header>
+				<Card.Title>Proof of Sats</Card.Title>
+				<Card.Description>I have Sats, and I want a slice of a Rocket's satflow.</Card.Description>
+			</Card.Header>
+			<img src="{base}/_sats.png" width="100%" alt="Proof of Sats" />
+			<Card.Content class="grid gap-4 !pt-2">
+				<div>
+					<ul class="ml-6 list-disc [&>li]:mt-2">
+						<li>Nostrocket is the most efficient way (ever) to invest in emerging technologies</li>
+						<li>
+							Every Sat goes directly to a contributor for solving a problem in the critical path
+							towards (more) revenue
+						</li>
+						<li>
+							Revenue goes straight from the paying customer's wallet into your wallet as an equity
+							holder
+						</li>
+						<li>No KYC, ever.</li>
+					</ul>
+				</div>
+			</Card.Content>
+			<Card.Footer class="mt-auto">
+				<Button class="w-full">Read More</Button>
+			</Card.Footer>
+		</Card.Root>
+		<Card.Root class="flex h-full w-full flex-col">
+			<Card.Header>
+				<Card.Title>Launch a new Rocket</Card.Title>
+				<Card.Description
+					>I'm working on my own project and I want other people to join me.</Card.Description
+				>
+			</Card.Header>
+			<img src="{base}/_rocket.png" width="100%" alt="Launch a new Rocket" />
+			<Card.Content class="grid gap-4 !pt-2">
+				<div>
+					<ul class="ml-6 list-disc [&>li]:mt-2">
+						<li>
+							Attract the people you need to make your project a success (despite your lack of
+							funding)
+						</li>
+						<li>
+							Nostrocket obsoletes the need for financial accounting, capital raising, compliance,
+							business plans, etc.
+						</li>
+						<li>
+							Nostrocket is radically meritocratic and fair to project creators, contributors, and
+							investors, for the entire life of the project
+						</li>
+					</ul>
+				</div>
+			</Card.Content>
+			<Card.Footer class="mt-auto">
+				<CreateNewRocket />
+			</Card.Footer>
+		</Card.Root>
 	</div>
 </div>
