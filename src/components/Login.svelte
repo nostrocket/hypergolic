624e738705f1f54769f5583c26546f2b0aa4ad4b
Bob <dv4xd@proton.me>
2024-09-02 21:36:02 +0800

    problem: new users can't experiment with a new pubkey

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -3,45 +3,107 @@
 	import { Button } from '@/components/ui/button';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import { Avatar } from '@nostr-dev-kit/ndk-svelte-components';
+	import UseTemporaryAccount from './UseTemporaryAccount.svelte';
+	import ConnectToBunker from './ConnectToBunker.svelte';
+	import * as Dialog from '$lib/components/ui/dialog';
 	import { NDKNip07Signer } from '@nostr-dev-kit/ndk';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
+	import { connectToBunker, loginWithNsec, purgeSignedInStorage } from '@/login';
+	import { pubkey } from '@/stores/session';
 
-	onMount(() => {
-		if (localStorage.getItem('signed-in')) {
-			nip07();
+	$: isLoading = true;
+
+	onMount(async () => {
+		const signedIn = localStorage.getItem('signed-in');
+		if (signedIn) {
+			try {
+				if (signedIn === 'nip07') {
+					await loginByExtension();
+				} else if (signedIn === 'nsec') {
+					const nsec = localStorage.getItem('signed-in-nsec');
+					if (nsec) {
+						const signer = loginWithNsec(nsec);
+						if (signer instanceof Error) {
+							purgeSignedInStorage();
+						} else {
+							$pubkey = (await signer.user()).pubkey;
+							$ndk.signer = signer;
+						}
+					} else {
+						purgeSignedInStorage();
+					}
+				} else if (signedIn === 'nip46') {
+					const token = localStorage.getItem('signed-in-nsecbunker-token');
+					if (token) {
+						const signer = await connectToBunker(token);
+						$pubkey = (await signer.user()).pubkey;
+						$ndk.signer = signer;
+					} else {
+						purgeSignedInStorage();
+					}
+				} else {
+					purgeSignedInStorage();
+				}
+			} catch (error) {
+				console.error('Error during auto-login:', error);
+				purgeSignedInStorage();
+			}
 		}
+		isLoading = false;
 	});
-	$: pubkey = '';
 
-	async function nip07() {
-		document.body.appendChild(document.createElement('script')).src =
-			'https://unpkg.com/window.nostr.js/dist/window.nostr.js';
-
+	async function loginByExtension() {
 		try {
 			const signer = new NDKNip07Signer();
 			const user = await signer.blockUntilReady();
 
 			if (user) {
-				pubkey = user.pubkey;
+				$pubkey = user.pubkey;
 				$ndk.signer = signer;
-				$ndk = $ndk;
-				localStorage.setItem('signed-in', 'true');
+				localStorage.setItem('signed-in', 'nip07');
 			}
 		} catch (e) {
 			alert(e);
 		}
 	}
+
+	async function logout() {
+		purgeSignedInStorage();
+		$ndk.signer = undefined;
+		$pubkey = '';
+		await goto(`${base}/`);
+	}
 </script>
 
 {#if !$ndk.signer}
-	<Button on:click={nip07} class="shrink-0">Sign in</Button>
+	<Dialog.Root>
+		<Dialog.Trigger class="shrink-0">
+			<Button disabled={isLoading}>
+				{#if isLoading}
+					Loading...
+				{:else}
+					Log In
+				{/if}
+			</Button>
+		</Dialog.Trigger>
+		<Dialog.Content class="flex flex-col gap-4 p-4">
+			<Dialog.Header>
+				<Dialog.Title>Log In</Dialog.Title>
+			</Dialog.Header>
+			<Button on:click={loginByExtension} class="w-full">Browser Extension</Button>
+			<div class="w-full space-y-2">
+				<UseTemporaryAccount />
+				<ConnectToBunker />
+			</div>
+		</Dialog.Content>
+	</Dialog.Root>
 {:else}
 	<DropdownMenu.Root>
 		<DropdownMenu.Trigger asChild let:builder>
 			<Button builders={[builder]} variant="secondary" size="icon" class="shrink-0 rounded-full">
-				<Avatar ndk={$ndk} {pubkey} class="h-10 w-10 flex-none rounded-full object-cover" />
+				<Avatar ndk={$ndk} pubkey={$pubkey} class="h-10 w-10 flex-none rounded-full object-cover" />
 				<span class="sr-only">Toggle user menu</span>
 			</Button>
 		</DropdownMenu.Trigger>
@@ -53,14 +115,16 @@
 					goto(`${base}/sellmerits`);
 				}}>Sell Merits</DropdownMenu.Item
 			>
-			<DropdownMenu.Item on:click={() => {
-				goto(`${base}/buymerits`);
-			}}>Buy Merits</DropdownMenu.Item>
+			<DropdownMenu.Item
+				on:click={() => {
+					goto(`${base}/buymerits`);
+				}}>Buy Merits</DropdownMenu.Item
+			>
 			<DropdownMenu.Separator />
 			<DropdownMenu.Item>Settings</DropdownMenu.Item>
 			<DropdownMenu.Item>Support</DropdownMenu.Item>
 			<DropdownMenu.Separator />
-			<DropdownMenu.Item>Logout</DropdownMenu.Item>
+			<DropdownMenu.Item on:click={logout}>Logout</DropdownMenu.Item>
 		</DropdownMenu.Content>
 	</DropdownMenu.Root>
 {/if}
